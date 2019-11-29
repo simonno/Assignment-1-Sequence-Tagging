269abb0424f721406bc04d4a671af9586e18d107
Noam Simon <simonoam@gmail.com>
2019-11-29 23:24:12 +0200

    close to work

--- HMMTag .py
+++ HMMTag .py
@@ -1,5 +1,7 @@
 import sys
 
+import numpy as np
+
 import dicUtils
 
 START = 'START'
@@ -9,45 +11,64 @@ num_word_count = 0
 unk_tsg_list = []
 
 
-def get_score(word, tag, prev_tag, prev_prev_tag):
-    return dicUtils.compute_e(dic_e, dic_q, word, tag) * dicUtils.compute_q(dic_q, num_word_count, prev_prev_tag,
-                                                                            prev_tag, tag, 0.9, 0.09, 0.01)
+# def get_score(word, tag, prev_tag, prev_prev_tag):
+#     return dicUtils.compute_e(dic_e, dic_q, word, tag) * dicUtils.compute_q(dic_q, num_word_count, prev_prev_tag,
+#                                                                             prev_tag, tag, 0.9, 0.09, 0.01)
 
 
 def viterbi(input_file_name, hmm_viterbi_predictions):
     # Y = []
 
-    with open(input_file_name, 'r') as input_file:
+    with open(input_file_name, 'r') as input_file, open(hmm_viterbi_predictions, 'w') as output_file:
         for line in input_file:
             words = line.split('\n')[0].split(' ')
             prev_tags = [START]
             prev_prev_tags = [START]
             v_table = {(0, START, START): 1}
             bq = {}
-            for i in range(len(words)):
-                w_i = words[i]
+            for i in range(1, len(words) + 1):
+                w_i = words[i - 1]
                 possible_tags_w_i = dicUtils.possible_tags(dic_e, w_i)
                 for r in possible_tags_w_i:
                     for t1 in prev_tags:
-                        max_prob = 0
-                        # max_prob = -np.math.inf
+                        max_prob = -np.math.inf
                         max_tag = t1
                         for t2 in prev_prev_tags:
-                            prob = v_table.get((i, t2, t1)) * get_score(w_i, r, t1, t2)
+                            prob = v_table.get((i - 1, t2, t1)) * dicUtils.get_score(dic_e, dic_q, num_word_count, w_i,
+                                                                                     r, t1, t2)
                             if prob > max_prob:
                                 max_prob = prob
                                 max_tag = t2
 
-                        v_table[(i + 1, t1, r)] = max_prob
-                        bq[(i + 1, t1, r)] = max_tag
+                        v_table[(i, t1, r)] = max_prob
+                        bq[(i, t1, r)] = max_tag
 
                 prev_prev_tags = prev_tags
                 prev_tags = possible_tags_w_i
 
-            print(v_table)
-            # for i in reversed(range(len(words))):
-            #     for [r,t] in v_table[i]:
-            #         for r in v_table[i][t]:
+            n = len(words)
+            possible_tags_w_i = dicUtils.possible_tags(dic_e, words[-1])
+            prev_tags = dicUtils.possible_tags(dic_e, words[-2])
+            y_n = ''
+            y_n_1 = ''
+            max_prob = -np.math.inf
+            for r in possible_tags_w_i:
+                for t in prev_tags:
+                    prob = v_table.get((n, t, r))
+                    if max_prob < prob:
+                        max_prob = prob
+                        y_n = r
+                        y_n_1 = t
+
+            tagged_line = '{0}/{1} {2}/{3}\n'.format(words[-2], y_n_1, words[-1], y_n)
+            for i in reversed(range(1, n - 1)):
+                y_i = bq[(i + 2, y_n_1, y_n)]
+                tagged_line = '{0}/{1} {2}'.format(words[i], y_i, tagged_line)
+                y_n = y_n_1
+                y_n_1 = y_i
+
+            print(tagged_line)
+            output_file.write(tagged_line)
 
 
 def main(input_file_name, q_mle, e_mle, hmm_viterbi_predictions, extra_file_name):

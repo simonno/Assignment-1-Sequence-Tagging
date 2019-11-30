f8860690db2797e23adc430faff2cbb6d66c803e
8945 <chen.eliyahou@gmail.com>
2019-12-01 00:21:30 +0200

    Merge branch 'master' of https://github.com/simonno/Assignment-1-Sequence-Tagging
    # Conflicts:
    #	Accuracy.py

---
+++ Utils/FeaturesUtils.py
@@ -0,0 +1,23 @@
+from Utils.DictUtils import DictUtils
+
+
+class FeaturesUtils:
+    @staticmethod
+    def num_of_instaces(word, dict_e):
+        return DictUtils.get_value(dict_e, word)
+
+    @staticmethod
+    def suffix(word, num=4):
+        if num > len(word):
+            num = len(word)
+        return [word[-i:] for i in range(1, num + 1)]
+
+    @staticmethod
+    def prefix(word, num=4):
+        if num > len(word):
+            num = len(word)
+        return [word[:i] for i in range(num)]
+
+    @staticmethod
+    def contains_number(word):
+        

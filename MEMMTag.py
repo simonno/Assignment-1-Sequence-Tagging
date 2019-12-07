2d0a1a9efcf255ee873f9f5abda6843afdae7fdf
Noam Simon <simonoam@gmail.com>
2019-12-07 20:30:35 +0200

    add file

--- MEMMTag.py
+++ MEMMTag.py
@@ -1,13 +1,12 @@
-import pickle
 import sys
 from datetime import datetime
 
 import numpy as np
+from scipy import sparse
 
 from Utils.DictUtils import DictUtils
 from Utils.FeaturesUtils import FeaturesUtils
 from Utils.FileUtils import FileUtils
-from Utils.WordSignature import WordSignatures
 
 START = 'START'
 UNK = '*unk*'
@@ -17,31 +16,32 @@ def get_words(sentence):
     return sentence.split(' ')
 
 
-def create_features_numeric_format(word_features, features_map):
-    numeric_format_vector = np.zeros(len(features_map.items()))
-    for feature_key, feature_value in word_features.items():
-        feature = '{0}={1}'.format(feature_key, feature_value)
-        if feature in features_map.keys():
-            numeric_format_vector[features_map[feature]] = 1
-    return numeric_format_vector
+def create_features_numeric_format(all_word_features, features_map):
+    col = list()
+    row = list()
+    data = list()
+    for i in range(len(all_word_features)):
+        word_features = all_word_features[i]
+        for feature_key, feature_value in word_features.items():
+            feature = '{0}={1}'.format(feature_key, feature_value)
+            if feature in features_map.keys():
+                row.append(i)
+                col.append(features_map[feature])
+                data.append(1)
+    return sparse.csr_matrix((data, (row, col)), shape=(len(all_word_features), len(features_map.items())))
 
 
-def get_predict_prob_of_word(word_features, clf, feature_map):
-    # clf works only on numbers format
-    features_numeric_format = create_features_numeric_format(word_features, feature_map)
-    predict_prob = clf.predict_log_prob([features_numeric_format.tolist()])[0]
-    return predict_prob
+def get_predict_prob_of_word(all_word_features, clf, feature_map):
+    numeric_format_matrix = create_features_numeric_format(all_word_features, feature_map)
+    predict_probabilities = clf.predict_proba(numeric_format_matrix)
+    return predict_probabilities
 
 
-def max_prob_and_tag(v_table, i, words_list, prev_prediction, prev_prev_predictions, counters_dict, clf, features_map):
+def max_prob_and_tag(v_table, i, prev_prediction, prev_prev_predictions, predict_probabilities, prob_index):
     max_prob = -np.inf
     max_tag = prev_prev_predictions[0]
-    for prev_prev_prediction in prev_prev_predictions:
-        word_features = FeaturesUtils.get_word_features(i, words_list, [prev_prev_prediction, prev_prediction],
-                                                        DictUtils.is_rare(counters_dict, words_list[i]))
-
-        predict_prob = get_predict_prob_of_word(word_features, clf, features_map)
-        prob = v_table[(i - 1, prev_prev_prediction, prev_prediction)] + predict_prob
+    for prev_prev_prediction, probs in zip(prev_prev_predictions, predict_probabilities):
+        prob = v_table[(i - 1, prev_prev_prediction, prev_prediction)] + probs[prob_index]
         if prob > max_prob:
             max_prob = prob
             max_tag = prev_prev_prediction
@@ -49,15 +49,23 @@ def max_prob_and_tag(v_table, i, words_list, prev_prediction, prev_prev_predicti
     return max_prob, max_tag
 
 
-def calc_v_table_at_i(v_table, bq, i, words_list, possible_prediction_dict, prev_predictions, prev_prev_predictions,
-                      counters_dict, clf, features_map):
-    for word, tags in possible_prediction_dict.items():
-        for tag in tags:
-            for prev_prediction in prev_predictions:
-                max_prob, max_tag = max_prob_and_tag(v_table, i, words_list, prev_prediction, prev_prev_predictions,
-                                                     counters_dict, clf, features_map)
-                v_table[(i, prev_prediction, tag)] = max_prob
-                bq[(i, prev_prediction, tag)] = max_tag
+def calc_v_table_at_i(v_table, bq, i, tuples_dict):
+    for key in tuples_dict.keys():
+        tag = key[0]
+        prev_prediction = key[1]
+        prev_prev_prediction = key[2]
+        if (i - 1, prev_prev_prediction, prev_prediction) not in v_table:
+            continue
+
+        score = v_table[(i - 1, prev_prev_prediction, prev_prediction)] * tuples_dict[key]
+
+        if (i, prev_prediction, tag) not in v_table:
+            v_table[(i, prev_prediction, tag)] = score
+            bq[(i, prev_prediction, tag)] = prev_prev_prediction
+
+        elif v_table[(i, prev_prediction, tag)] < score:
+            v_table[(i, prev_prediction, tag)] = score
+            bq[(i, prev_prediction, tag)] = prev_prev_prediction
 
 
 def get_backtrack(v_table, bq, words):
@@ -85,28 +93,49 @@ def get_backtrack(v_table, bq, words):
     return tagged_line
 
 
-def get_word_signatures_tag(word, dict_e, unk_tag_list):
-    signatures = WordSignatures.get_word_signatures(word)
-    if signatures == [word.lower()]:
-        return {UNK: unk_tag_list}
-    else:
-        signatures_tags = dict()
-        for signature in signatures:
-            signatures_tags[signature] = DictUtils.possible_tags(signature, dict_e)
-        return signatures_tags
+def get_tag_list(index, prev_predictions, prev_prev_predictions):
+    tags_list = list()
+    tags_list.append(prev_prev_predictions)
+    tags_list.append(prev_predictions)
+    for i in range(index - 1):
+        tags_list.insert(0, '')
+    return tags_list
 
 
-def possible_tags(word, word_tag_dict, unk_tag_list):
-    words_tags = word_tag_dict[word.lower()]
-    words_tags = DictUtils.possible_tags(word, dict_e)
+def get_word_features_list(i, words, prev_predictions, prev_prev_predictions, counters_dict):
+    all_word_features = list()
+    for prev_prediction in prev_predictions:
+        for prev_prev_prediction in prev_prev_predictions:
+            word_features = FeaturesUtils.get_word_features(i, words,
+                                                            get_tag_list(i, prev_prediction, prev_prev_prediction),
+                                                            DictUtils.is_rare(counters_dict, words[i]))
+            all_word_features.append(word_features)
+    return all_word_features
 
-    if len(words_tags) == 0:
-        return get_word_signatures_tag(word, word_tag_dict, unk_tag_list)
-    else:
-        return {word: words_tags}
+
+def get_best_tuples(tuples_dict, num_of_best_items=50):
+    best_tuples_dict = dict()
+    for best_item in sorted(tuples_dict.items(), key=lambda item: item[1], reverse=True)[:num_of_best_items]:
+        best_tuples_dict[best_item[0]] = best_item[1]
+
+    return best_tuples_dict
 
 
-def viterbi(sentences, features_map, counters_dict, word_tag_dict, unk_tag_list, clf):
+def get_tuples_dict(i, words, prev_predictions, prev_prev_predictions, counters_dict, clf, features_map, classes):
+    tuples_dict = dict()
+    all_word_features = get_word_features_list(i, words, prev_predictions, prev_prev_predictions, counters_dict)
+    predict_probabilities = get_predict_prob_of_word(all_word_features, clf, features_map)
+
+    for prev_prediction in prev_predictions:
+        for prev_prev_prediction, probs in zip(prev_prev_predictions, predict_probabilities):
+            for tag, prob in zip(classes, probs):
+                tuples_dict[(tag, prev_prediction, prev_prev_prediction)] = prob
+
+    return get_best_tuples(tuples_dict)
+    # return tuples_dict
+
+
+def viterbi(sentences, features_map, counters_dict, clf, classes):
     tagged_text = list()
     for sentence in sentences:
         words = get_words(sentence)
@@ -115,19 +144,33 @@ def viterbi(sentences, features_map, counters_dict, word_tag_dict, unk_tag_list,
         bq = dict()
         prev_predictions = [START]
         prev_prev_predictions = [START]
-        for i in range(len(words)):
-            possible_prediction_dict = possible_tags(words[i], word_tag_dict, unk_tag_list)
-            calc_v_table_at_i(v_table, bq, i, words, possible_prediction_dict, prev_predictions, prev_prev_predictions,
-                              counters_dict, clf, features_map)
+        start = datetime.now()
 
-            prev_prev_predictions = prev_predictions
-            prev_predictions = []
-            for list_tags in possible_prediction_dict.values():
-                prev_predictions += list_tags
+        for i in range(len(words)):
+            # print(words[i])
+            # print(prev_prev_predictions)
+            # print(prev_predictions)
+            tuples_dict = get_tuples_dict(i, words, prev_predictions, prev_prev_predictions, counters_dict, clf,
+                                          features_map, classes)
+
+            calc_v_table_at_i(v_table, bq, i, tuples_dict)
+            end = datetime.now()
+
+            prev_predictions = list()
+            prev_prev_predictions = list()
+            for tuple in tuples_dict.keys():
+                if tuple[0] not in prev_predictions:
+                    prev_predictions.append(tuple[0])
+                if tuple[1] not in prev_prev_predictions:
+                    prev_prev_predictions.append(tuple[1])
 
         tagged_line = get_backtrack(v_table, bq, words)
 
         print(tagged_line)
+        end = datetime.now()
+        print('{1} - Running Time: {0}'.format(end - start, sentences.index(sentence)))
+        print()
+
         tagged_text.append(tagged_line)
     return tagged_text
 
@@ -135,16 +178,14 @@ def viterbi(sentences, features_map, counters_dict, word_tag_dict, unk_tag_list,
 def main(input_file_name, model_file_name, feature_map_file, output_file_name):
     start = datetime.now()
 
+    clf, vec = FileUtils.read_logistic_regression_model(model_file_name)
+    classes = clf.classes_.tolist()
     sentences = FileUtils.read_lines(input_file_name)
     feature_map_lines = FileUtils.read_lines(feature_map_file)
     features_map, counters_dict, word_tag_dict, unk_tag_list = DictUtils.create_features_dicts(feature_map_lines)
 
-    model_file = open(model_file_name, 'rb')
-    (clf, vec) = pickle.load(model_file)
-    tagged_text = viterbi(sentences, features_map, counters_dict, word_tag_dict, unk_tag_list, clf)
+    tagged_text = viterbi(sentences, features_map, counters_dict, clf, classes)
     FileUtils.write_tagged_text(output_file_name, tagged_text)
-    end = datetime.now()
-    print('Running Time: {0}'.format(end - start))
 
     end = datetime.now()
     print('Running Time: {0}'.format(end - start))
